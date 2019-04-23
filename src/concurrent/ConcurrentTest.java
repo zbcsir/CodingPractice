ca8cd41dea98a2bde9a9b80edc54673cd6162d06
张本才 <570724307@qq.com>
2019-04-23 17:55:51 +0800

    将ConcurrentTest移至concurrent包

---
+++ src/concurrent/ConcurrentTest.java
@@ -0,0 +1,50 @@
+package concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ConcurrentTest {
+    public static void main(String[] args) {
+//        System.out.println(9/3*3);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("Thread run");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        executorService.shutdownNow();
+        System.out.println("Main run");
+
+        SynchronizedExample se = new SynchronizedExample();
+        SynchronizedExample se2 = new SynchronizedExample();
+        executorService.execute(() -> se.func2());
+        executorService.execute(() -> se2.func2());
+//        ThreadPoolExecutor t = new ThreadPoolExecutor(5,)
+
+    }
+
+
+}
+
+class SynchronizedExample2 {
+    public void func1() {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    synchronized void func2() {
+//        for (int i = 0; i < 10; i++) {
+//            System.out.print(i + " ");
+//        }
+        for (int i = 0; i < 10; i++) {
+            System.out.print((i + 10) + " ");
+        }
+    }
+}
