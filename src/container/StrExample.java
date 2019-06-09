b3afb4c19e52bce083f83d55a77ec6a05ccbf8ee
张本才 <570724307@qq.com>
2019-06-09 11:13:33 +0800

    String常量池练习demo

--- src/container/StrExample.java
+++ src/container/StrExample.java
@@ -1,5 +1,6 @@
 package container;
 
+// 字符串常量池测试
 public class StrExample {
     private void testStr() {
         String str1 = "abc"; //常量池
@@ -10,8 +11,25 @@ public class StrExample {
         System.out.println(str1 == str3);
     }
 
+    private void internExample() {
+        String s1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(s1.intern() == s1); //true
+        String s12 = new String("计算机软件");
+        System.out.println(s12.intern() == s12); //false
+        String s13 = "计算机软件";
+        System.out.println(s13.intern() == s13); //
+
+        String s2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(s2.intern() == s2); //false
+        String s22 = new String("java");
+        System.out.println(s22.intern() == s22); //false
+        String s23 = "java";
+        System.out.println(s23.intern() == s23); //
+    }
+
     public static void main(String[] args) {
         StrExample strObj = new StrExample();
-        strObj.testStr();
+//        strObj.testStr();
+        strObj.internExample();
     }
 }
