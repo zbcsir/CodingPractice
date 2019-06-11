c59302ec7eef0077870ffcf35a95f9b2bd174a1d
张本才 <570724307@qq.com>
2019-06-11 18:31:53 +0800

    Leetcode 最长连续序列

--- src/algorithm/listarray/LongestConsecutive.java
+++ src/algorithm/listarray/LongestConsecutive.java
@@ -1,5 +1,7 @@
 package algorithm.listarray;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -32,7 +34,8 @@ public class LongestConsecutive {
     }
 
     // 基于哈希set
-    public int longestConsecutiveByHash(int[] nums) {
+    private int longestConsecutiveByHash(int[] nums) {
+        long startTime = System.currentTimeMillis();
         if (nums.length == 0)
             return 0;
         int cnt = 1;
@@ -51,14 +54,75 @@ public class LongestConsecutive {
             }
             cnt = Math.max(cnt, cnt_tmp);
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time : " + (endTime - startTime));
         return cnt;
+    }
+
+    // 基于排序
+    private int longestConsecutiveBySort(int[] nums) {
+        long startTime = System.currentTimeMillis();
+        if (nums.length == 0) return 0;
+        int cnt = 1;
+        int maxCnt = 1;
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i-1] == 1) {
+                ++ cnt;
+                maxCnt = Math.max(maxCnt, cnt);
+            }
+            else if (nums[i] != nums[i-1])
+                cnt = 1;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time : " + (endTime - startTime));
+        return maxCnt;
+    }
+
+    // 基于并查集 好像并不如排序快，就不做了
+    private int longestConsecutiveByUF(int[] nums) {
+        if (nums.length <= 1)  return nums.length;
+
+        return 0;
+    }
+
+    // 返回e的顶层父节点
+    private int find(HashMap<Integer, Integer> uf, int e) {
+        if (uf.get(e) == e) return e;
+        int parent = find(uf, uf.get(e));
+        uf.put(e, parent);
+        return parent;
+    }
+
+    // 另一种基于哈希set的方法，遍历的时候不断删除来减少重复遍历
+    private int longestConsecutiveByHash2(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int maxLen = 1;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        for (int num : nums) {
+            int cur = num;
+            int maxLenTmp = 0;
+            while (set.remove(cur)) -- cur;
+            maxLenTmp += (num - cur);
+            cur = num + 1;
+            while (set.remove(cur)) ++ cur;
+            maxLenTmp += (cur - num - 1);
+            maxLen = maxLenTmp > maxLen ? maxLenTmp : maxLen;
+        }
+        return maxLen;
     }
 
     public static void main(String[] args) {
         LongestConsecutive lc = new LongestConsecutive();
         int[] nums = {100, 4, 200, 1, 3, 2};
-        System.out.println(lc.contains(nums, 4));
-        int cnt = lc.longestConsecutive(nums);
-        System.out.println(cnt);
+        int cnt = lc.longestConsecutiveByHash(nums);
+        System.out.println("res : " + cnt);
+        int cnt2 = lc.longestConsecutiveBySort(nums);
+        System.out.println("res : " + cnt2);
+        int cnt3 = lc.longestConsecutiveByHash2(nums);
+        System.out.println("res : " + cnt3);
     }
 }
