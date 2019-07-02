df7b49564607b603a9f0d9f2f017e66abbab8c56
张本才 <570724307@qq.com>
2019-07-02 21:23:49 +0800

    增加Leetcode 31 下一个排列

--- src/algorithm/listarray/Permute.java
+++ src/algorithm/listarray/Permute.java
@@ -1,15 +1,22 @@
 package algorithm.listarray;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Permute {
-    // Leetcode 46 全排列
-    // 给定一个没有重复数字的序列，返回其所有可能的全排列
+
+    /**
+     * Leetcode 46 全排列
+     * 给定一个没有重复数字的序列，返回其所有可能的全排列
+     * @param nums  给定的序列
+     * @return  所有可能的全排列
+     */
     private List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums.length == 0)
+        if (nums.length == 0) {
             return res;
+        }
         recursion(nums, 0, res);
         return res;
     }
@@ -44,10 +51,60 @@ public class Permute {
         }
     }
 
+    private void printArray(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " -> ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Leetcode 31 下一个排列
+     * @param nums  当前排列
+     */
+    private void nextPermutation(int[] nums) {
+        int len = nums.length;
+        int i = len - 2;
+        while ((i >= 0) && (nums[i] >= nums[i+1])) {
+            -- i;
+        }
+        if (i >= 0) {
+            int j = len - 1;
+            while ((j >= 0) && (nums[j] <= nums[i])) {
+                -- j;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void swap(int[] a, int i, int j) {
+        a[i] = a[i] ^ a[j];
+        a[j] = a[i] ^ a[j];
+        a[i] = a[i] ^ a[j];
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            ++ i;
+            -- j;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
+        int[] nums = {3,2,1};
+        int[] nums2 = {1,1,5};
+        int[] nums3 = {1,2,3};
         Permute p = new Permute();
-        List<List<Integer>> res = p.permute(nums);
-        p.printRes(res);
+//        List<List<Integer>> res = p.permute(nums);
+//        p.printRes(res);
+        p.nextPermutation(nums);
+        p.nextPermutation(nums2);
+        p.nextPermutation(nums3);
+        p.printArray(nums);
+        p.printArray(nums2);
+        p.printArray(nums3);
     }
 }
