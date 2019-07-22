61b27ea59f5a24952e1da369531aa5fb2746a9a5
张本才 <570724307@qq.com>
2019-07-22 16:52:31 +0800

    N皇后问题

--- src/algorithm/trace/NQueen.java
+++ src/algorithm/trace/NQueen.java
@@ -6,14 +6,13 @@ import java.util.List;
 import java.util.Set;
 
 public class NQueen {
-    List<List<String>> res;
-    List<String> resOne;
-    List<Integer> colNoRes;
-    Set<Integer> xminusy, xplusy, cols;
-    int CONSTINT;
+    private List<List<String>> res;
+    private List<String> resOne;
+    private List<Integer> colNoRes;
+    private Set<Integer> xminusy, xplusy, cols;
 
     // LeetCode 51题，N皇后
-    public List<List<String>> solveNQueens(int n) {
+    private List<List<String>> solveNQueens(int n) {
         res = new ArrayList<>();
         resOne = new ArrayList<>();
         colNoRes = new ArrayList<>();
@@ -21,27 +20,13 @@ public class NQueen {
         xplusy = new HashSet<>(2 * n);
         cols = new HashSet<>(n);
         search(n, 0, xminusy, xplusy, cols, colNoRes);
-//        System.out.println(colNoRes.size());
-//        for (int colno : colNoRes) {
-//            System.out.println(colno);
-//        }
-//            System.out.println(res.size());
-//            for (List<String> one : res) {
-////                System.out.println(one.size());
-//                for (String s : one) {
-//                    System.out.println(s);
-//                }
-//                System.out.println();
-//            }
         return res;
     }
 
-
-    public void search(int n, int row, Set<Integer> minus, Set<Integer> plus, Set<Integer> cols, List<Integer> colNo){
+    private void search(int n, int row, Set<Integer> minus, Set<Integer> plus, Set<Integer> cols, List<Integer> colNo){
         if (row == n) {
             List<String> one = new ArrayList<>(n);
             for (int c : colNo) {
-//                System.out.print(c + "\t");
                 StringBuilder tmp = new StringBuilder();
                 for (int j = 0; j < n; j++) {
                     if (j == c) {
@@ -50,13 +35,8 @@ public class NQueen {
                     }
                     tmp.append(".");
                 }
-//                System.out.println(tmp.toString());
                 one.add(tmp.toString());
             }
-//            for (String restmp : one) {
-//                System.out.println(restmp);
-//            }
-//            System.out.println();
             res.add(one);
             return;
         }
@@ -74,15 +54,14 @@ public class NQueen {
             cols.remove(i);
             minus.remove(row - i);
             plus.remove(row + i);
-//            System.out.println("col remove : " + colNo.get(colNo.size()-1));
             colNo.remove(colNo.size()-1);
         }
     }
 
 
     // LeetCode 52题，N皇后问题2
-    int num = 0;
-    public int totalNQueensOld(int n) {
+    private int num = 0;
+    private int totalNQueensOld(int n) {
         colNoRes = new ArrayList<>();
         xminusy = new HashSet<>(2 * n);
         xplusy = new HashSet<>(2 * n);
@@ -92,7 +71,7 @@ public class NQueen {
         return num;
     }
 
-    public void search2(int n, int row, Set<Integer> minus, Set<Integer> plus, Set<Integer> cols, List<Integer> colNo){
+    private void search2(int n, int row, Set<Integer> minus, Set<Integer> plus, Set<Integer> cols, List<Integer> colNo){
         if (row == n) {
             num++;
             return;
@@ -111,14 +90,13 @@ public class NQueen {
             cols.remove(i);
             minus.remove(row - i);
             plus.remove(row + i);
-//            System.out.println("col remove : " + colNo.get(colNo.size()-1));
             colNo.remove(colNo.size()-1);
         }
     }
 
     // N皇后位运算求解
-    int count = 0;
-    public void search3 (int n, int row, int col, int ld, int rd) {
+    private int count = 0;
+    private void search3 (int n, int row, int col, int ld, int rd) {
         if (row >= n) {
             count += 1;
         }
@@ -130,7 +108,7 @@ public class NQueen {
         }
     }
 
-    public int totalNQueens(int n) {
+    private int totalNQueens(int n) {
         search3(n, 0, 0, 0, 0);
         System.out.println(count);
         return count;
